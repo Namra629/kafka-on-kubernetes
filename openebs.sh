4ada660a7addc7d78419f1b2ce675eaaefe226b2
Namra629 <hafizanamrarasheed@gmail.com>
2025-12-25 04:54:38 +0500

    Update openebs.sh

--- openebs.sh
+++ openebs.sh
@@ -7,3 +7,22 @@ kubectl apply -f https://openebs.github.io/charts/openebs-operator.yaml
 kubectl get sc
 kubectl get pods -n openebs --watch
 
+# Creating a custom storage class with our own custom path set on worker node
+
+cat << EOF > openebsstorageclass.yaml
+
+apiVersion: storage.k8s.io/v1
+kind: StorageClass
+metadata:
+  name: openebs-hostpath-custom
+  annotations:
+    storageclass.kubernetes.io/is-default-class: "true"
+provisioner: openebs.io/local
+volumeBindingMode: WaitForFirstConsumer
+reclaimPolicy: Delete
+parameters:
+  basePath: /var/local-hostpath
+
+EOF
+
+
